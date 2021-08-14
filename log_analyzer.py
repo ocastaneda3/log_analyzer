c6b59a6039279b79504f59d795ec5f5de3b07ee1
Oscar Castaneda <ocastaneda3@csu.fullerton.edu>
2021-08-13 19:43:02 -0700

    Add files via upload

--- log_analyzer.py
+++ log_analyzer.py
@@ -110,80 +110,36 @@ class log_stats():
                 print( '%-15s %8d' %( _t[0][:40], _t[1] ) )
         pass
 
-# function - parse_arguments
-#   - Parse arguments from one or more strings from the command line.
-#   - Print appropriate error messages when input file or flags are missing
-def parse_arguments( ):
-    _parser = argparse.ArgumentParser(prog = 'sudo log_analysis.py')
-
-    # Add file requirement argument
-    _required = _parser.add_argument_group('required arguments')
-    _required.add_argument( 
-        'filename',
-        help='input file name',
-        default = None,
-        type=argparse.FileType('r'),
-        nargs='+'
-    )
-    
-    # Custom n number print
-    _parser.add_argument(
-        '-n',
-        type = int,
-        metavar='N',
-        default = 5,
-        help = 'print top N'
-    )
-    
-    # Print full IP list w/ occurences
-    _parser.add_argument(
-        '-f', '--full',
-        action = 'store_true',
-        default = False,
-        help = 'print full ip list w/ number of occurences'
-    )
-
-    # Used with [-f][--full] to print ip only list
-    _parser.add_argument(
-        '-c', '--clean',
-        action = 'store_true',
-        default = False,
-        help = 'to be used with [-f] to print clean list'
-    )
-
-    # Debug flag
-    _parser.add_argument(
-        '-d', '--debug',
-        action = 'store_true',
-        default = False,
-        help = 'enable debugging'
-    )
-
-    # Check if auth.log file is being used
-    if not any( 'auth.log' in _args for _args in sys.argv ) and not any( '-h' in _args for _args in sys.argv ):
-        print( 'error: missing filename\n' )
-        _parser.parse_args(['-h'])
-        
-    _args = _parser.parse_args( )
-
-    # Check if [--full] flag is set when [--clean] flag is set
-    if _args.clean and not _args.full :
-        print( 'error: missing [-f][--full] flag\n' )
-        _parser.parse_args(['-h'])
-
-    return _args
-
 # ------------------------
 #           Main
 # ------------------------
 if __name__ == '__main__':
+    # Setup command line argument parsing.
+    parser = argparse.ArgumentParser(description='Simple tool to pull Failed Password attempts from SSH AUTH Logs')
 
-    args = parse_arguments()
+    parser.add_argument('filename', nargs='+', help='input file name')
+    parser.add_argument('-n', metavar='N', default=5, help='print top N')
+    parser.add_argument('-f','--full',  action='store_true', help='print full ip list w/ number of occurences')
+    parser.add_argument('-c','--clean', action='store_true', help='print clean list and to be used with [-f] to')
+    parser.add_argument('-d','--debug', action='store_true', help='enable debugging')
 
-    log_obj = log_stats( [_file.name for _file in args.filename], args.n, args.full, args.clean, args.debug )
+    parser.print_usage = parser.print_help
+    
+    # Parse command line arguments.
+    args = parser.parse_args( )
 
+    # Check if [--full] flag is set when [--clean] flag is set
+    if args.clean and not args.full :
+        sys.stderr.write( 'error: missing [-f][--full] flag\n\n' )
+        parser.print_help()
+
+    # Create log_stat object
+    log_obj = log_stats( args.filename, args.n, args.full, args.clean, args.debug )
+
+    # Parse files
     log_obj.parse_file()
 
+    # Print default output or full list
     if args.full:
         log_obj.print_ip_list()
     else:
